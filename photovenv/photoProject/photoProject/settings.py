95171f86403ed32fecaf99ec12aa0092f2a5642e
ANUSHA P V <121385976+Anusha629@users.noreply.github.com>
2023-09-30 17:27:42 +0530

    Update settings.py

--- photovenv/photoProject/photoProject/settings.py
+++ photovenv/photoProject/photoProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gtjl+llhypsg*3@tbpidh+q*je#t649ab4xu!u+r_wd22t339^'
+SECRET_KEY = 'Your_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
